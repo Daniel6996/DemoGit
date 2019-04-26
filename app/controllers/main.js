4501f8a7849f621c54cbef209f37c143c82b65d1
Daniel6996 <tranthai6367@gmail.com>
2019-04-26 20:44:20 +0700

    sua file

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -1,67 +1,81 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var nguoiDungService = new NguoiDungService();
 
     layNguoiDungService();
 
-   function getInput(title, btnTitle , btnId){
-    $(".modal-title").html(title);
-    var footer=`
+    function getInput(title, btnTitle, btnId) {
+        $(".modal-title").html(title);
+        var footer = `
         <button id="${btnId}" class="btn btn-success">${btnTitle}</button>
         <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
     `
-    $(".modal-footer").html(footer);
+        $(".modal-footer").html(footer);
     }
 
-    $("#btnThemNguoiDung").click(function(){
-       getInput("Thêm người dùng","Thêm","btnThem")
+    $("#btnThemNguoiDung").click(function () {
+        getInput("Thêm người dùng", "Thêm", "btnThem")
     })
-    $("body").delegate(".btnSua","click",function(){
-        getInput("Sửa người dùng","Sửa","btnThem")
+    $("body").delegate(".btnSua", "click", function () {
+        getInput("Sửa người dùng", "Sửa", "btnThem")
     })
 
-    $("body").delegate("#btnThem","click",function(){
-       var taiKhoan = $('#TaiKhoan').val();
-       var hoTen = $('#HoTen').val();
-       var matKhau = $('#MatKhau').val();
-       var email = $('#Email').val();
-       var sdt = $('#SoDienThoai').val();
-       var loaiNguoiDung = $('#loaiNguoiDung').val();
+    $("body").delegate("#btnThem", "click", function () {
+        var taiKhoan = $('#TaiKhoan').val();
+        var hoTen = $('#HoTen').val();
+        var matKhau = $('#MatKhau').val();
+        var email = $('#Email').val();
+        var sdt = $('#SoDienThoai').val();
+        var loaiNguoiDung = $('#loaiNguoiDung').val();
 
-       var nguoiDung = new NguoiDung(taiKhoan,matKhau,hoTen,email,sdt, loaiNguoiDung);
-       console.log(nguoiDung);
-       nguoiDungService.themNguoiDung(nguoiDung);
+        var nguoiDung = new NguoiDung(taiKhoan, matKhau, hoTen, email, sdt, loaiNguoiDung);
+        console.log(nguoiDung);
+        nguoiDungService.themNguoiDung(nguoiDung);
 
     })
-    $("#txtTimKiem").keyup(function(){
+    $("#txtTimKiem").keyup(function () {
         var taiKhoan = $('txtTimKiem').val();
         var mangTimKiem = [];
         mangTimKiem = nguoiDungService.timKiemNguoiDung(taiKhoan);
         taoBang(mangTimKiem);
 
     })
+    $("body").delegate(".btnSua", "click", function () {
+        var taiKhoan = $(this).data('taiKhoan');
+        // var viTri =nguoiDungService.layNguoiDungService
+        // var DanhSachNguoiDung = JSON.parse(localStorage.getItem("DanhSachNguoiDung"));
+        var nguoiDung = nguoiDungService.layThongTinNguoiDung(taiKhoan);
+        
 
-    $("body").delegate(".btnXoa","click",function(){
-        var taiKhoan = $(this).data('taikhoan');
+        $('#TaiKhoan').val(taiKhoan);
+        $('#HoTen').val(nguoiDung.HoTen);
+        $('#MatKhau').val(nguoiDung.MatKhau);
+        $('#Email').val(nguoiDung.Email);
+        $('#SoDienThoai').val(nguoiDung.SoDT);
+        $('#loaiNguoiDung').val(nguoiDung.loaiNguoiDung);
+    })
+
+    $("body").delegate(".btnXoa", "click", function () {
+        var taiKhoan = $(this).data('taiKhoan');
         nguoiDungService.xoaNguoiDung(taiKhoan);
     })
 
-    function layNguoiDungService(){
+    function layNguoiDungService() {
         nguoiDungService.layDanhSachNguoiDung()
-        .done(function(result){
-            
-            taoBang(result);
-            localStorage.setItem("DanhSachNguoiDung", JSON.stringify(result));
-        })
-        .fail(function(err){
-            console.log(err);
-        })
+            .done(function (result) {
+
+                taoBang(result);
+                localStorage.setItem("DanhSachNguoiDung", JSON.stringify(result));
+            })
+            .fail(function (err) {
+                console.log(err);
+            })
 
     }
 });
-function taoBang(DanhSachNguoiDung){
+function taoBang(DanhSachNguoiDung) {
     var tblBody = "";
-    DanhSachNguoiDung.map(function(item,index){ // map thay thế cho for (gần như foreach  )
-        
+    DanhSachNguoiDung.map(function (item, index) { // map thay thế cho for (gần như foreach  )
+
         tblBody += `
             <tr>
                 <td>${index + 1}</td>
@@ -72,14 +86,14 @@ function taoBang(DanhSachNguoiDung){
                 <td>${item.SoDT}</td>
                 <td>${item.TenLoaiNguoiDung}</td>
                 <td>
-                    <button  class="btn btn-success btnSua" data-toggle="modal" data-target="#myModal">Sửa</button>
+                    <button  class="btn btn-success btnSua" data-taiKhoan="${item.TaiKhoan} data-toggle="modal" data-target="#myModal">Sửa</button>
                     <button  class="btn btn-danger btnXoa" data-taikhoan="${item.TaiKhoan}">Xóa</button>
                 </td>
     
             </tr>
             `
-        
+
     })
-   
+
     $("#tblDanhSachNguoiDung").html(tblBody);
 }
--- app/services/NguoiDungService.js
+++ app/services/NguoiDungService.js
@@ -1,52 +1,76 @@
-function NguoiDungService(){
+function NguoiDungService() {
     this.DSND = [];
-    this.layDanhSachNguoiDung = function(){
-       return $.ajax({
+    this.layDanhSachNguoiDung = function () {
+        return $.ajax({
             url: "http://svcy.myclass.vn/api/QuanLyTrungTam/DanhSachNguoiDung",
             type: "GET"
         })
-       
+
     }
-    this.themNguoiDung = function(nguoiDung){
+    this.themNguoiDung = function (nguoiDung) {
         $.ajax({
             url: "http://svcy.myclass.vn/api/QuanLyTrungTam/ThemNguoiDung",
             type: "POST",
             data: nguoiDung
-            
+
         })
-        .done(function(a){
-            if(a === "tai khoan da ton tai !"){
-                alert(a);
-            }else{
-                location.href = "";
-            }
-        })
-        .fail(function(b){
-            console.log(b);
-        })
+            .done(function (a) {
+                if (a === "tai khoan da ton tai !") {
+                    alert(a);
+                } else {
+                    location.href = "";
+                }
+            })
+            .fail(function (b) {
+                console.log(b);
+            })
 
     }
-    this.xoaNguoiDung = function(taiKhoan){
-        $.ajax ({
+    this.xoaNguoiDung = function (taiKhoan) {
+        $.ajax({
             url: `http://svcy.myclass.vn/api/QuanLyTrungTam/XoaNguoiDung/${taiKhoan}`,
             type: "DELETE",
 
         })
-        .done(function(result){
-            console.log(result);
-        })
-        .fail(function(err){
-            console.log(err);
-        })
+            .done(function (result) {
+                console.log(result);
+            })
+            .fail(function (err) {
+                console.log(err);
+            })
     }
-    this.timKiemNguoiDung = function(chuoiTimKiem){
+    this.timKiemNguoiDung = function (chuoiTimKiem) {
         var mangTimKiem = []
-      var dsnd =  JSON.parse(localStorage.getItem("danhSachNguoiDung"));
-      dsnd.map(function(){
-          if(item.TaiKhoan.toLowerCase().indexOf(chuoiTimKiem.toLowerCase()>-1)){
-            mangTimKiem.push(item);
-          }
-      })
-      return mangTimKiem;
+        var dsnd = JSON.parse(localStorage.getItem("danhSachNguoiDung"));
+        dsnd.map(function () {
+            if (item.TaiKhoan.toLowerCase().indexOf(chuoiTimKiem.toLowerCase() > -1)) {
+                mangTimKiem.push(item);
+            }
+        })
+        return mangTimKiem;
+    }
+    this.layViTriNguoiDung = function (taiKhoan) {
+        var danhSachNguoiDung = JSON.parse(localStorage.getItem("DanhSachNguoiDung"));
+        var viTri;
+        // danhSachNguoiDung.map(function (item) {
+        //     if (item.TaiKhoan === taiKhoan) {
+        //         viTri = index;
+        //         return viTri;
+
+        //     }
+
+        // })
+        return danhSachNguoiDung.findIndex(function(item){
+            return item.TaiKhoan = taiKhoan;
+        })
+       
+
+    }
+    this.layThongTinNguoiDung = function(taiKhoan){
+        var danhSachNguoiDung = JSON.parse(localStorage.getItem("DanhSachNguoiDung"));
+      return  danhSachNguoiDung.find(function(item){
+           return item.TaiKhoan === taiKhoan;
+        })
     }
 }
+
